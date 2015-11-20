ed98d4aa13357037c5caa6b9824ae21ee7efde9d
Oliver Chick <oc243@cam.ac.uk>
2015-11-20 13:43:48 +0000

    Allow all users to run sudo vmutil, check permissions of vmutil

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,9 @@
 class vmutil (
   $hosts,
   $xe_path,
+  $user_vms,
   $password_file = "/etc/vmutil-password",
+  $sudo = "sudo vmutil",
 ) {
 
   $packagelist = ['mercurial', 'dialog']
@@ -18,6 +20,14 @@ class vmutil (
     source   => 'https://bitbucket.org/mas90/vmutil',
     owner    => 'root',
     group    => 'root'
+  }
+  file { '/usr/local/lib/vmutil/vmutil':
+    ensure => 'file',
+    mode   => '0755',
+  }
+  file { '/usr/local/lib/vmutil/xe-where-is-my-master':
+    ensure => 'file',
+    mode   => '0755',
   }
   file { '/usr/local/bin/vmutil':
     ensure => 'link',
@@ -36,11 +46,21 @@ class vmutil (
     target => $password_file,
   }
   file { $password_file:
-    mode => 600
+    mode => '0600',
   }
 
 
   file { '/etc/vmutil.conf':
     content => template("vmutil/vmutil.conf.erb")
   }
+
+  sudoers::allowed_command{ 'ALL':
+      command          => '/usr/local/bin/vmutil, /usr/local/bin/xe-where-is-my-master',
+      user             => 'ALL',
+      run_as           => 'root',
+      require_password => false,
+      comment          => "Allow anyone to run vmutil",
+  }
+
+
 }
